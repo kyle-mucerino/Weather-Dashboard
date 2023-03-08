06d07618f257183c46400cad80ad03b793f1d92d
kyle-mucerino <kyle.mucerino@gmail.com>
2023-03-08 09:48:20 -0800

    updated README

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+Screenshot: https://user-images.githubusercontent.com/124935227/223790041-afe2859c-56b6-4536-9329-7e289e403629.png
+
+Deployed site: https://kyle-mucerino.github.io/Weather-Dashboard/
+
+
 # Weather-Dashboard
--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-var weatherApiRootUrl = "https://api.openweathermap.org";
+var weatherApiRootUrl =
+  "https://api.openweathermap.org&634c860cea6d4fc60d7b5d8fa5e16a03";
 var weatherApiKey = "634c860cea6d4fc60d7b5d8fa5e16a03";
 var searchHistory = [];
 
 var searchForm = document.querySelector('#search-form');
 var searchInput = document.querySelector('#search-input');
+var searchHistory = document.querySelector('.search-history')
 var today = document.querySelector('#today');
 var FiveDayForecast = document.querySelector('#5-day');
 
-function getWeatherToday(city, weather) {
+function renderWeatherToday(city, weather) {
   var date = dayjs().format("MM-DD-YYYY HH:mm:ss"); 
   var tempF = weather.main.temp;
   var windMph = weather.wind.speed;
@@ -22,20 +24,9 @@ function getWeatherToday(city, weather) {
   var tempEl = document.createElement('p');
   var windEl = document.createElement("p"); 
   var humidityEl = document.createElement("p");
-  
-  card.setAttribute('class', card);
-  tempEl.setAttribute('class', 'card-text');
-  windEl.setAttribute('class', 'card-text');
-  humidityEl.setAttribute('class', 'card-text');
-  weatherIcon.setAttribute('src', weatherIconUrl);
-  weatherIcon.setAttribute('alt', weatherIconDescription);
-
-  heading.textContent = `${city} (${date})`;
-  heading.append(weatherIcon);
-  tempEl.textContent = `Temp: ${tempF}°F`;
-  windEl.textContent = `Wind: ${windMph}mph`;
-  humidityEl.textContent = `Humidity: ${humidity}%`;
-  card.append(heading, tempEl. windEl, humidityEl);
-  today.innerHTML = '';
-  today.append(card);
 }
+
+
+
+$("#today").text(dayjs().format("dddd, MMMM D, YYYY"));
+
